076a24ed4143d9951aec95f4b11254479532c6da
Brooke Fox <brookefox@Brookes-MacBook-Air-2.local>
2018-03-04 11:39:56 -0800

    simplify bowling

--- 1-php-basics/bowling.php
+++ 1-php-basics/bowling.php
@@ -4,7 +4,7 @@
   function bowl($number_of_bowls = 1) {
     $number_of_pins = array();
     for ($i = 1; $i <= $number_of_bowls; $i++) {
-      array_push($number_of_pins, random_bowl());
+      array_push($number_of_pins, rand(0, 10));
     }
     if ($number_of_bowls == 10) {
       return summarize_game($number_of_pins);
@@ -13,13 +13,9 @@
     }
   }
 
-  function random_bowl() {
-    return rand(0, 10);
-  }
-
-  function sum($total, $i) {
-    $total += $i;
-    return $total;
+  function sum($sum, $i) {
+    $sum += $i;
+    return $sum;
   }
 
   function summarize_game($pins_per_bowl) {
