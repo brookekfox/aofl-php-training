bca771b096403995d520ba74a9738c59f5f1c9e8
Brooke Fox <brooke.fox@aofl.com>
2018-03-08 14:39:59 -0800

    adding fractions

--- 2-php-advanced/adding-fractions.php
+++ 2-php-advanced/adding-fractions.php
@@ -1,6 +1,10 @@
 <?php
 
   function add_fractions($frac_1, $frac_2) {
+    if ($frac_1["denom"] == 0 || $frac_2["denom"] == 0) {
+      echo "cannot have 0 in denominator\n";
+      return false;
+    }
     $new_denominator = lcm($frac_1["denom"], $frac_2["denom"]);
     $frac_1_numerator = $frac_1["num"] * ($new_denominator / $frac_1["denom"]);
     $frac_2_numerator = $frac_2["num"] * ($new_denominator / $frac_2["denom"]);
@@ -43,11 +47,13 @@
   $one_eleventh = array("num" => 1, "denom" => 11);
   $five_sixths = array("num" => 5, "denom" => 6);
   $twentyone_fifteenths = array("num" => 21, "denom" => 15);
+  $bad = array("num" => 1, "denom" => 0);
 
   add_fractions($three_fifths, $twentyone_fifteenths); // 30/15 => 2/1
   add_fractions($one_half, $one_third); // 5/6
   add_fractions($three_halves, $nine_fourths); // 15/4
   add_fractions($three_fifths, $one_eleventh); // 38/55
   add_fractions($five_sixths, $twentyone_fifteenths); // 67/30
+  add_fractions($one_half, $bad);
 
 ?>
