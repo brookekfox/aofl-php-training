797397b23991c12e71bbd09aa75366b1c2b3e2a3
Brooke Fox <brooke.fox@aofl.com>
2018-03-12 15:19:01 -0700

    blackjack

--- 2-php-advanced/blackjack.php
+++ 2-php-advanced/blackjack.php
@@ -1,7 +1,10 @@
 <?php
 
+  const UPPER_LIMIT = 21;
+  const STAY_LIMIT = 17;
+
   function build_deck() {
-    $values = array("2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "1");
+    $values = array("2", "3", "4", "5", "6", "7", "8", "9", "10", "10", "10", "10", "1");
     $suits  = array("spades", "hearts", "diamonds", "clubs");
     $cards = array();
     foreach ($suits as $suit) {
@@ -13,30 +16,56 @@
     return $cards;
   }
 
+  function check_game($player_1, $player_2) {
+    echo "player 1 received " . $player_1["total"] . " points\n";
+    echo "player 2 received " . $player_2["total"] . " points\n";
+    if ($player_1["total"] > UPPER_LIMIT) {
+      echo "player 1 busts\n";
+    }
+    if ($player_2["total"] > UPPER_LIMIT) {
+      echo "player 2 busts\n";
+    }
+
+    if (
+      $player_1["total"] <= UPPER_LIMIT &&
+      $player_2["total"] <= UPPER_LIMIT &&
+      $player_1["total"] == $player_2["total"]
+    ) {
+      echo "draw\n";
+    } else {
+      if (
+        ($player_1["total"] <= UPPER_LIMIT && $player_1["total"] > $player_2["total"]) ||
+        ($player_1["total"] <= UPPER_LIMIT && $player_2["total"] > UPPER_LIMIT)
+      ) {
+        echo "player 1 wins\n";
+        return $player_1;
+      }
+      if (
+        ($player_2["total"] <= UPPER_LIMIT && $player_1["total"] < $player_2["total"]) ||
+        ($player_2["total"] <= UPPER_LIMIT && $player_1["total"] > UPPER_LIMIT)
+      ) {
+        echo "player 2 wins\n";
+        return $player_2;
+      }
+    }
+    return false;
+  }
+
   function play_blackjack($dealer, $player_1, $player_2) {
     $card_number = 0;
-    while ($player_1["total"] < 21) {
+    while ($player_1["total"] <= STAY_LIMIT) {
       $card = $dealer["cards"][$card_number];
       array_push($player_1["cards"], $card);
       $player_1["total"] += $card;
       $card_number++;
     }
-    while ($player_2["total"] < 21) {
+    while ($player_2["total"] <= STAY_LIMIT) {
       $card = $dealer["cards"][$card_number];
       array_push($player_2["cards"], $card);
       $player_2["total"] += $card;
       $card_number++;
     }
-    if ($player_1["total"] == $player_2["total"]) {
-      echo "draw\n";
-      return true;
-    } elseif ($player_1["total"] > $player_2["total"]) {
-      echo "player 1 wins with " . $player_1["total"] . " points\n";
-      return true;
-    } elseif ($player_1["total"] < $player_2["total"]) {
-      echo "player 2 wins with " . $player_2["total"] . " points\n";
-      return true;
-    }
+    return check_game($player_1, $player_2);
   }
 
   $player_1 = array(
